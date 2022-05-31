0ab28f3fa199bd3b957aea52e9de06cf1f702e23
Yum <jallygaming@gmail.com>
2022-05-31 23:10:52 +0300

    Add ShadowflameSkull sprite

--- Content/Items/Accessories/ShadowflameSkull.js
+++ Content/Items/Accessories/ShadowflameSkull.js
@@ -3,6 +3,11 @@ import {RequiemPlayer} from "../../RequiemPlayer.js";
 import {RequiemGlobalItem} from "../../RequiemGlobalItem.js";
 
 export class ShadowflameSkull extends ModItem {
+    constructor() {
+        super();
+        this.Texture = `Items/Accessories/${this.constructor.name}`;
+    }
+    
     SetDefaults() {
         this.Item.value = RequiemGlobalItem.Rarity5BuyPrice;
         this.Item.rare = 5;

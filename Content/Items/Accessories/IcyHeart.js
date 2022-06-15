11a54b2d0ca2f0254eec424da80045347d5e2209
Yum <jallygaming@gmail.com>
2022-06-15 18:26:55 +0300

    Add Icy Heart sprite

--- Content/Items/Accessories/IcyHeart.js
+++ Content/Items/Accessories/IcyHeart.js
@@ -2,6 +2,11 @@
 import {RequiemPlayer} from "../../RequiemPlayer.js";
 
 export class IcyHeart extends ModItem {
+    constructor() {
+        super();
+        this.Texture = `Items/Accessories/${this.constructor.name}`;
+    }
+
     SetDefaults() {
         this.Item.rare = 11;
         this.Item.value = ModItem.sellPrice(0, 12, 50, 0);

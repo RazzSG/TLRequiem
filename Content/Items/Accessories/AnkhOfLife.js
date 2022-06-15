10df905bcd5230f4514aebb8544a345d7cb02ff4
Yum <jallygaming@gmail.com>
2022-06-15 22:56:27 +0300

    Add Ankh Of Life sprite

--- Content/Items/Accessories/AnkhOfLife.js
+++ Content/Items/Accessories/AnkhOfLife.js
@@ -3,7 +3,16 @@ import {RequiemGlobalItem} from "../../RequiemGlobalItem.js";
 import {RequiemPlayer} from "../../RequiemPlayer.js";
 
 export class AnkhOfLife extends ModItem {
+    constructor() {
+        super();
+        this.Texture = `Items/Accessories/${this.constructor.name}`;
+        this.FrameCount = 60;
+        this.TicksPerFrame = 70;
+    }
+    
     SetDefaults() {
+        this.Item.width = 22;
+        this.Item.height = 33;
         this.Item.accessory = true;
         this.Item.value = RequiemGlobalItem.Rarity5BuyPrice;
         this.Item.rare = 5;
